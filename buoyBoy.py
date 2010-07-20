1f084d8395a3e0cda956ed7bcd206bf8ecc6955d
Charlie Sharpsteen <source@sharpsteen.net>
2010-07-20 02:05:32 -0700

    Finished setting up script arguments

--- buoyBoy.py
+++ buoyBoy.py
@@ -38,7 +38,8 @@ def processArgs():
 
   # Positional arguments- these are not identified by a flag.  Rather their meaning is
   # inferred from their position in the command line.
-  parser.add_argument( 'buoyNum', metavar = 'Buoy#', type = str,
+  parser.add_argument( 'buoyNum', metavar = 'Buoy#', type = int,
+                       choices = [46022, 46212, 46244],
                        help = 'The number of the NDBC buoy for which you wish to fetch data.' )
 
   parser.add_argument( 'startTime', metavar = 'StartTime', type = ISO_datestring,
@@ -47,18 +48,19 @@ def processArgs():
   parser.add_argument( 'stopTime', metavar = 'StopTime', type = ISO_datestring,
                         help = '''The end of the time range for which data is to be downloaded. 
                         Uses the same format as described above.''' ) 
+
   args = parser.parse_args()
   return args
+
 
 if __name__ == '__main__':
   """
   This is the actual script part.  Building a script file this way allows it to be used
   as both a command line tool and a python library.  Then other Python scripts can import
-  functions from this one.
+  functions from this one without running the script.
   """
   
   args = processArgs()
+
   print "Hello, world!\n"
-  print args.__dict__
-  print args.be_verbose
 
