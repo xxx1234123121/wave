bfb1399b1ef397d48ca6e8d88d6e2ffa0da61e50
Charlie Sharpsteen <source@sharpsteen.net>
2010-07-23 23:51:20 -0700

    Added support for storing NDBC buoys as Source records
    NDBC buoys may now be stored and retrieved from the tblSource in the
    database.  A function has also been added to retrieve the UUID
    associated with a buoy.

--- py/lib/wavecon/NDBC.py
+++ py/lib/wavecon/NDBC.py
@@ -12,6 +12,8 @@ Data Center (`NDBC`_).
 .. _NDBC: http://www.ndbc.noaa.gov/
 
 """
+
+
 import datetime
 
 import urllib
@@ -22,9 +24,32 @@ import re
 import functools
 import itertools
 
+from wavecon import DBman
+from wavecon.config import DBconfig as _DBconfig
+
 #---------------------------------------------------------------------
-#  Metadata
+#  Metadata and Other Constants
 #---------------------------------------------------------------------
+
+BUOY_META = {
+
+  '46022' : {
+    'location' : ( 40.749, -124.577 ),
+    'type' : 'NDBC'
+  },
+
+  '46212' : {
+    'location' : ( 40.753, -124.313 ),
+    'type' : 'SCRIPPS'
+  },
+
+  '46244' : {
+    'location' : ( 40.888, -124.357 ),
+    'type' : 'SCRIPPS'
+  }
+
+}
+
 
 #---------------------------------------------------------------------
 #  Data Retrieval
@@ -38,7 +63,8 @@ def fetchFromNDBC( buoyNum, startTime, stopTime, dataType,
   fetchData = functools.partial( NDBCGetData, 
     buoyNum = buoyNum, dataType = dataType )
 
-  dataSets = [ NDBCrawToRecords(data) for data in [fetchData( year ) for year in timeSpan]
+  dataSets = [ NDBCrawToRecords(data) for data in 
+    [fetchData( year ) for year in timeSpan]
     if NDBCGaveData(data) ]
 
   # dataSets contains a list of records- one list for each year.
@@ -102,8 +128,55 @@ def NDBCrawToRecords( rawData ):
 
 
 #---------------------------------------------------------------------
+#  Database Interaction 
+#---------------------------------------------------------------------
+def getBuoyFromDB( buoyNum ):
+  Buoy = DBman.accessTable( _DBconfig, 'tblsource' )
+  session = DBman.startSession( _DBconfig )
+
+  buoy = session.query(Buoy)\
+      .filter( Buoy.srcname == getBuoyName( buoyNum ) ).first()
+
+  if buoy:
+    session.close()
+    return buoy
+  else:
+    # A record for this buoy does not exist in the DB. Create it.
+    buoy = Buoy( srcName = getBuoyName( buoyNum ) )
+
+    session.add( buoy )
+    session.commit()
+    session.refresh( buoy )
+    session.close()
+
+    return buoy
+
+def getBuoyID( buoyNum ):
+  id = getBuoyFromDB( buoyNum ).srcid
+  return id
+
+#---------------------------------------------------------------------
 #  Utility Functions
 #---------------------------------------------------------------------
+def getBuoyName( buoyNum ):
+  buoyNum = str( buoyNum )
+  try:
+    name = "{}-{}".format( BUOY_META[buoyNum]['type'], buoyNum )
+  except:
+    print "There exists no buoy with the number: {}".format( buoyNum )
+    raise
+
+  return name
+
+
+def getBuoyLoc( buoyNum, asWKT = False ):
+  buoyLoc = BUOY_META[ str(buoyNum) ]
+  if asWKT:
+    return "POINT({} {})".format(*buoyLoc)
+  else:
+    return buoyLoc
+
+
 def isInsideTimespan( aDate, startTime, stopTime ):
   if startTime <= aDate and stopTime >= aDate:
     return True
