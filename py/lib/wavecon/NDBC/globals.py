49ae83062c01ecd1503192d9f56ce1d1956ab871
Charlie Sharpsteen <source@sharpsteen.net>
2010-08-16 16:36:12 -0700

    Seperated NDBC global variables
    Both the data fetching module and the database interface module need to
    access global variables like BUOY_META.  They have been separated into
    their own submodule so that both modules have equal access.

---
+++ py/lib/wavecon/NDBC/globals.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+
+# Custom objects
+Location = namedtuple( 'Location', 'lon lat' )
+FrequencySpectra = namedtuple( 'FrequencySpectra', 'datetime density' )
+
+# Metadata setup
+BUOY_META = {
+
+  '46022' : {
+    'location' : Location( 40.749, -124.577 ),
+    'type' : 'NDBC'
+  },
+
+  '46212' : {
+    'location' : Location( 40.753, -124.313 ),
+    'type' : 'SCRIPPS'
+  },
+
+  '46244' : {
+    'location' : Location( 40.888, -124.357 ),
+    'type' : 'SCRIPPS'
+  }
+
+}
