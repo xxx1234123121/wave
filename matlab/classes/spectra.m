e99334213923708e9a8dd4d18f55a54c70d89ffb
Colin Sheppard <sheppardc@Waveconnect-Mac-Pro.local>
2010-10-06 17:50:51 -0700

    Developing matlab spectra class.

---
+++ matlab/classes/spectra.m
@@ -0,0 +1,14 @@
+classdef spectra
+    %SPECTRA A class for representing one or two dimensional surface wave
+    %spectra.
+    %   Detailed explanation goes here
+    
+    properties
+        test=5;
+    end
+    
+    methods
+    end
+    
+end
+
