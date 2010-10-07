b5fe96eae8ec0a14606455a6be252b06a2f829b2
Colin Sheppard <sheppardc@Waveconnect-Mac-Pro.local>
2010-10-07 10:23:26 -0700

    Had some changes to db and elsewhere unrelated to matlab but in the matlab branch, so commiting before a merge to master.

--- matlab/classes/spectrum.m
+++ matlab/classes/spectrum.m
@@ -1,13 +1,34 @@
-classdef spectra
+classdef spectrum
     %SPECTRA A class for representing one or two dimensional surface wave
     %spectra.
     %   Detailed explanation goes here
     
     properties
-        test=5;
+        % TIME AND LOCATION
+        datetime;   % datevec
+        location;   % x,y GIS coordinates
+        projection; % associated GIS projection
+        
+        % SPECTRA
+        spec;       % the 1D or 2D matrix of spectral values
+        freqbin;    % frequency bins
+        dirbin;     % direction bins, leave as NULL if 1D
+        
+        % BULK STATISTICS
+        hs;         % significant wave height
+        tp;         % peak period
+        te;         % energy period
     end
     
     methods
+        function spec = spectrum(datetime,location,projection,spec,...
+            freqbin,dirbin)
+            spec.datetime   = datevec(datetime,'yyyy-mm-dd HH:MM:SS');
+            spec.location   = location;
+            spec.projection = projection;
+            spec.freqbin    = freqbin;
+            if nargin<5,spec.dirbin = dirbin; end
+        end
     end
     
 end
