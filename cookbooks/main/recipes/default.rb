5b080b00583da6f77e93a4764f8c575492266855
Charlie Sharpsteen <source@sharpsteen.net>
2010-11-07 20:20:34 -0800

    Added Pip and associated resources
    The pip instalation specification is quite hacky---it relies on
    `easy_install-2.7` being present in the path.
    
    The rest of the files add support so that Pip may be used by Chef as a provider
    of Python packages.  The provider and resource files are taken from Christopher
    Peplin's repository as discussed in Chef issue 973:
    
        http://tickets.opscode.com/browse/CHEF-973
    
    It looks like a native pip provider may show up in the 0.10 release of Chef.
    
    Modifications had to be made in order for the pip provider to be able to inherit
    from the EasyInstall class outside of the Chef install location.  This was
    acheieved with some tricks borrowed from Corey Donohoe's Smeagol repository
    which pulls similar tricks for Homebrew on OS X:
    
        https://github.com/atmos/smeagol

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,4 +3,12 @@ puts "Bootstrapping Development Environment."
 include_recipe "git"
 include_recipe "gfortran"
 include_recipe "python"
+include_recipe "pip"
+
+pipRoot = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "pip"))
+require pipRoot + "/providers/pip"
+require pipRoot + "/resources/pip_package"
+
+pip_package "sphinx"
+pip_package "flask"
 
