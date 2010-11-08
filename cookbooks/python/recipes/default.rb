f01058da06397ce7aec0050d1c857ec44e4a1b4e
Charlie Sharpsteen <source@sharpsteen.net>
2010-11-07 19:41:26 -0800

    Added python to the loadout
    Custom Python recipie that installs Pytho 2.7 along with python-setuptools for
    easy_install.  Definitely only available on Debian-based systems.

---
+++ cookbooks/python/recipes/default.rb
@@ -0,0 +1,11 @@
+case node[:platform]
+when 'debian', 'ubuntu'
+  package 'python2.7'
+  package 'python-setuptools'
+else
+  puts <<-EOS.undent
+    Warning: Python deployment is not tested outside of Debian-based systems!
+      In particular, the installation of the pip python package manager may fail!
+  EOS
+  package 'python'
+end
