ae882259a8b0ece6e19e552fb3d9f1a920d2d446
Charlie Sharpsteen <source@sharpsteen.net>
2010-11-09 12:28:59 -0800

    Added SciPy
    Getting SciPy to build against Python 2.7 required some tweaks-- but it works
    now.

--- deployment/cookbooks/datalibs/recipes/default.rb
+++ deployment/cookbooks/datalibs/recipes/default.rb
@@ -1,6 +1,8 @@
 case node[:platform]
 when "debian", "ubuntu"
   package "libatlas-amd64sse3-dev"
+  package "libfftw3-dev"
+  package "libsuitesparse-dev"
   package "libhdf5-serial-dev"
 else
   puts "Warning: The waveconnect deployment is only battle tested on Ubuntu!"
