fd442163ae9e3dbea3775f449a8f09bd4e925f87
Charlie Sharpsteen <source@sharpsteen.net>
2010-11-09 13:50:04 -0800

    Improved cookbook organization

--- cookbooks/buildtools/recipes/default.rb
+++ cookbooks/buildtools/recipes/default.rb
@@ -1,7 +1,10 @@
 case node[:platform]
 when "debian", "ubuntu"
+  package "build-essential"
   package "gfortran"
+  package "cmake"
 else
   puts "Warning: The waveconnect deployment is only battle tested on Ubuntu!"
   package "gfortran"
+  package "cmake"
 end
