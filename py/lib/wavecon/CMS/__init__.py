2513d9d9fb9aab88d91d0139ecc864b839d02302
Charlie Sharpsteen <source@sharpsteen.net>
2010-12-16 09:55:52 -0800

    Finished cmcards parser
    The parser is now constrained to being line-oriented and no longer allows
    matches to spread accross line boundaries.  This probably matchs the way the
    Fortran routine in CMS reads the file---one line at a time.
    
    The parser seems to process the cmcards file for the Humboldt bay simulation
    just fine---however there could be lurking bungs.  All in all a declarative
    parser that fits in ~30 lines is way better than 50 lines of regular expressions
    that couldn't even process the whole file.

--- py/lib/wavecon/CMS/__init__.py
+++ py/lib/wavecon/CMS/__init__.py
@@ -1,1 +1,12 @@
+"""
+Overview
+--------
+
+This module provides tools for interacting with the CMS model.
+
+**Development Status:**
+  **Last Modified:** December 16, 2010 by Charlie Sharpsteen
+
+"""
 from .loadOutput import load_cms_data
+from .cmcards import cmcards_parser
--- py/lib/wavecon/CMS/cmcards.py
+++ py/lib/wavecon/CMS/cmcards.py
@@ -3,11 +3,11 @@ Overview
 --------
 
 This module provides functions for interacting with CMS cmcards files.
-Currently, it contains a parser for deconstructing the cmcards into a python
+Currently, it contains a parser for deconstructing a cmcards file into a python
 dict.
 
 **Development Status:**
-  **Last Modified:** December 15, 2010 by Charlie Sharpsteen
+  **Last Modified:** December 16, 2010 by Charlie Sharpsteen
 
 """
 
@@ -15,37 +15,75 @@ dict.
 #------------------------------------------------------------------------------
 #  Imports from Python 2.7 standard library
 #------------------------------------------------------------------------------
-from datetime import datetime
-from os import path
 
 
 #------------------------------------------------------------------------------
 #  Imports from third party libraries
 #------------------------------------------------------------------------------
+from pyparsing import( ParserElement, Literal, Keyword, Word, Group, Dict,
+    Combine, Optional, Suppress, ZeroOrMore, OneOrMore, nums, alphanums,
+    quotedString, restOfLine, lineEnd, removeQuotes)
 
 
 #------------------------------------------------------------------------------
 #  Imports from other CMS submodules
 #------------------------------------------------------------------------------
-from pyparsing import *
+
 
 #------------------------------------------------------------------------------
 #  cmcards parser
 #------------------------------------------------------------------------------
+
+# Token Transformation Functions
+#===============================
+def classifyAtom(tokens):
+  atom = tokens[0]
+  if atom == 'ON' or atom == 'YES':
+    return True
+  elif atom == 'OFF' or atom == 'NO':
+    return False
+  else:
+    return atom
+
+
+# Token Definitions
+#==================
+
+# Every component of the parser inherits from ParserElement. Setting the
+# whitespace characters here means that the parser will be line-oriented. By
+# default it would use ' \t\r\n' which means that the parser would continue
+# matching tokens accross line boundaries. Since the num_int, atom and
+# config_key tokens are similar, this behavior poses a problem.
+ParserElement.setDefaultWhitespaceChars(' \t')
+
 comment_char = Literal('!')
-jdate = Combine(Literal('0') + Word(nums))
-num_dble = Word(nums + '.').setParseAction(lambda tokens: float(tokens[0]))
-string_data = quotedString.setParseAction(removeQuotes)
-atom = Keyword('ON') | Keyword('OFF') | Keyword('EXPLICIT') | Keyword('IMPLICIT')
-
 config_key = Word(alphanums + '_')
-config_data = Group(OneOrMore(quotedString | jdate | num_dble | atom))
+jdate = Combine(Literal('0') + Word(nums))
+num_int = Word(nums).\
+  setParseAction(lambda tokens: int(tokens[0]))
+num_dble = Word(nums + '.').\
+  setParseAction(lambda tokens: float(tokens[0]))
+string_data = quotedString.\
+  setParseAction(removeQuotes)
+atom = Word(alphanums).\
+  setParseAction(classifyAtom)
+end_of_file = Keyword('END_PARAMETERS')
 
+
+# Parser Components
+#==================
+
+# The data contained on each line is a whitespace-delimited series of
+# components.  The config_data parser tries to match each of the components
+# listed below and returns the first match. In the case where a number could
+# match both the pattern for an integer and a float the longest match is used.
+# In the case of a tie, an integer is returned.  This prevents 3.75 from being
+# split into 3 and 0.75.
+config_data = Group(OneOrMore(quotedString | jdate | (num_int ^ num_dble) | atom))
+
+config_line = Group( config_key + config_data ) + Suppress(lineEnd)
 comment = comment_char + Optional(restOfLine)
-config_line = Group(config_key + config_data + Suppress(lineEnd))
 
 cmcards_parser = Dict(
-  ZeroOrMore(
-    config_line | Suppress(comment)
-  )
-)
+  ZeroOrMore( config_line | Suppress(comment) | Suppress(lineEnd) )
+) + Suppress(end_of_file)
